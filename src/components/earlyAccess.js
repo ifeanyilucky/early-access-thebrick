044cc2b153c3b54e11c26670a1127ee9f3384d47
ifeanyilucky <wfguru2017@gmail.com>
2022-01-01 09:31:01 +0100

    deleted and modified some files

---
+++ src/components/earlyAccess.js
@@ -0,0 +1,28 @@
+const EarlyAccess = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div class="subscribe">
+        <h2 className="fs-1">Join The Waitlist</h2>
+        <p class="subscribe__copy">
+          Be the first set of people to use Thebrick
+        </p>
+        <div class="form">
+          <input
+            type="email"
+            class="form__email"
+            placeholder="Enter your email address"
+          />
+          <button class="form__button">Request Access</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EarlyAccess;
